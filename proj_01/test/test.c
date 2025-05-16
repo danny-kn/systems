c3441490ce18be0c541db9b1b7b804b1a6afe6c5
Danny Kan <danny_kn@proton.me>
2025-05-15 20:14:44 -0400

    dev/ added a test.c file.

---
+++ proj_01/test/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(void) {
+  pid_t pid;
+  if ((pid = fork()) < 0) {
+    // fprintf(stderr, "\"fork()\" sys. call failed.\n");
+    perror("fork err");
+    exit(EXIT_FAILURE);
+  } else if (pid == 0) { // child process.
+    printf("child process: hello, world!\n");
+  } else { // parent process.
+    printf("parent process: hello, world!\n");
+  }
+  return EXIT_SUCCESS;
+}
