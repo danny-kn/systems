68f421c52d1f3cc53fb3457db78ab37b36103884
Danny Kan <danny_kn@proton.me>
2025-05-17 04:39:32 -0400

    dev/ added various files to implement a bitstream cipher.

---
+++ proj_05/bit_encode.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bit_encode.h"
+
+unsigned char cleartext[MAX_CHAR_LEN + 1] = {0};
+unsigned char ciphertext[MAX_CHAR_LEN + 1] = {0};
+
+int main(void) {
+  return EXIT_SUCCESS;
+}
---
+++ proj_05/bit_encode.h
@@ -0,0 +1,10 @@
+#ifndef BIT_ENCODE_H
+#define BIT_ENCODE_H
+
+#define MAX_CHAR_LEN 100
+
+unsigned char create_key();
+unsigned char rotate_left(unsigned char x);
+unsigned char rotate_right(unsigned char x);
+
+#endif
