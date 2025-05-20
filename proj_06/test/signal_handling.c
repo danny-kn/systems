4e0a937fd06d3431f5226fabcf45fd971f2e35ec
Danny Kan <danny_kn@proton.me>
2025-05-20 03:26:39 -0400

    dev/ added a makefile to a proj_06/ directory to automate the build process.

---
+++ proj_06/test/signal_handling.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include "signal_handling.h"
+
+int ct = 0;
+
+void sigint_handler(int sig) {
+  printf("SIGINT: %d\n", ++ct);
+}
+
+void sigquit_handler(int sig) {
+  printf("SIGQUIT: ct = %d\n", ct);
+  exit(EXIT_SUCCESS);
+}
+
+int main(void) {
+  signal(SIGINT, sigint_handler);
+  signal(SIGQUIT, sigquit_handler);
+  while (true) { }
+  return EXIT_SUCCESS;
+}
